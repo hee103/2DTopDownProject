874adb392bfb3e82dbbe189c09234ff0aa02e652
hee103 <gmlgml3213@gmail.com>
2025-02-17 10:04:11 +0900

    [Feat] 플레이어 이동 구현
    BaseController 스크립트 생성
    PlayerController 스크립트 생성

--- 2DTopDownProject/Assets/Scripts/Entity/PlayerController.cs
+++ 2DTopDownProject/Assets/Scripts/Entity/PlayerController.cs
@@ -2,17 +2,34 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
-public class PlayerController : MonoBehaviour
+public class PlayerController : BaseController
 {
-    // Start is called before the first frame update
-    void Start()
+    private Camera camera;
+
+    protected override void Start()
     {
-        
+        base.Start();
+        camera = Camera.main;
     }
 
-    // Update is called once per frame
-    void Update()
+    protected override void HandleAction()
     {
-        
+        float horizontal = Input.GetAxisRaw("Horizontal");
+        float vertial = Input.GetAxisRaw("Vertical");
+        movementDirection = new Vector2(horizontal, vertial).normalized;
+
+        Vector2 mousePosition = Input.mousePosition;
+        Vector2 worldPos = camera.ScreenToWorldPoint(mousePosition);
+        lookDirection = (worldPos - (Vector2)transform.position);
+
+        if(lookDirection.magnitude<0.9f)
+        {
+            lookDirection = Vector2.zero;
+
+        }
+        else
+        {
+            lookDirection = lookDirection.normalized;
+        }
     }
 }
